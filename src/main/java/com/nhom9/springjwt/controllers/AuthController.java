336c91b844eef436c2ab1ff7ba87949db9c53de2
Vu Nguyen <54635540+vniiphone@users.noreply.github.com>
2022-11-25 15:01:52 +0700

    User - ROLE - Brand

--- src/main/java/com/nhom9/springjwt/controllers/AuthController.java
+++ src/main/java/com/nhom9/springjwt/controllers/AuthController.java
@@ -20,14 +20,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nhom9.springjwt.models.ERole;
-import com.nhom9.springjwt.models.Role;
+// import com.nhom9.springjwt.models.ERole;
+// import com.nhom9.springjwt.models.Role;
 import com.nhom9.springjwt.models.User;
 import com.nhom9.springjwt.payload.request.LoginRequest;
 import com.nhom9.springjwt.payload.request.SignupRequest;
 import com.nhom9.springjwt.payload.response.JwtResponse;
 import com.nhom9.springjwt.payload.response.MessageResponse;
-import com.nhom9.springjwt.repository.RoleRepository;
+// import com.nhom9.springjwt.repository.RoleRepository;
 import com.nhom9.springjwt.repository.UserRepository;
 import com.nhom9.springjwt.security.jwt.JwtUtils;
 import com.nhom9.springjwt.security.services.UserDetailsImpl;
@@ -42,8 +42,8 @@ public class AuthController {
   @Autowired
   UserRepository userRepository;
 
-  @Autowired
-  RoleRepository roleRepository;
+  // @Autowired
+  // RoleRepository roleRepository;
 
   @Autowired
   PasswordEncoder encoder;
@@ -51,7 +51,8 @@ public class AuthController {
   @Autowired
   JwtUtils jwtUtils;
 
-  @CrossOrigin(origins = "http://127.0.0.1:5173")
+  // @CrossOrigin(origins = "http://127.0.0.1:5173")
+  @CrossOrigin(origins = "*", maxAge=3600)
   @PostMapping("/signin")
   public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 
@@ -59,20 +60,30 @@ public class AuthController {
         new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
     SecurityContextHolder.getContext().setAuthentication(authentication);
+    
     String jwt = jwtUtils.generateJwtToken(authentication);
+    System.out.println("======TEST 1=====> jwt = "+jwt);
+
 
     UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-    List<String> roles = userDetails.getAuthorities().stream()
-        .map(item -> item.getAuthority())
-        .collect(Collectors.toList());
+    System.out.println("======TEST 2=====> roles = "+userDetails.getRole().toString());
+    // String roleName = userDetails.getRole().toString();
+    // List<String> roles = userDetails.getAuthorities().stream()
+    //     .map(item -> item.getAuthority())
+    //     .collect(Collectors.toList());
 
-    return ResponseEntity.ok(new JwtResponse(jwt,
+    return ResponseEntity.ok(new JwtResponse(
+        jwt,
         userDetails.getId(),
         userDetails.getUsername(),
         userDetails.getEmail(),
-        roles));
+        userDetails.getRole().toString()));
   }
 
+ /**
+   * @param signUpRequest
+   * @return
+   */
   @PostMapping("/signup")
   public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
     if (userRepository.existsByUsername(signUpRequest.getUsername())) {
@@ -88,41 +99,42 @@ public class AuthController {
     }
 
     // Create new user's account
-    User user = new User(signUpRequest.getUsername(),
-        signUpRequest.getEmail(),
-        encoder.encode(signUpRequest.getPassword()));
+    User user = new User(signUpRequest.getUsername(), signUpRequest.getEmail(),  encoder.encode(signUpRequest.getPassword()) );
+       
+    // Set<String> strRoles = signUpRequest.getRole();
+    // Set<UserDetailsImpl> roles = new HashSet<>();
 
-    Set<String> strRoles = signUpRequest.getRole();
-    Set<Role> roles = new HashSet<>();
+  //  String roleName =  user.getRole().toString();
 
-    if (strRoles == null) {
-      Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-          .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-      roles.add(userRole);
-    } else {
-      strRoles.forEach(role -> {
-        switch (role) {
-          case "admin":
-            Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-            roles.add(adminRole);
+    // if (roleName == null) {
+    //  roles = userRepository.findByRole(user.getRole())
+    //       .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+    //   roles.add(roleName);
+    // } else {
+    //   roleName.forEach(user.getRole().toString())
+    //   {
+    //     switch (role) {
+    //       case "admin":
+    //         user.setRole(roleName) = roleRepository.findByName(ERole.ROLE_ADMIN)
+    //             .orElseThrow(().RuntimeException("Error: Role is not found."));
+    //         roles.add(adminRole);
 
-            break;
-          case "mod":
-            Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
-                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-            roles.add(modRole);
+    //         break;
+    //       case "user":
+    //       user.setRole(roleName) = roleRepository.findByName(ERole.ROLE_MODERATOR)
+    //             .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+    //         roles.add(modRole);
 
-            break;
-          default:
-            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-            roles.add(userRole);
-        }
-      });
-    }
-
-    user.setRoles(roles);
+    //         break;
+    //       default:
+    //         Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+    //             .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+    //         roles.add(userRole);
+    //     }
+    //   };
+    // }
+    
+    user.setRole("user");
     userRepository.save(user);
 
     return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
