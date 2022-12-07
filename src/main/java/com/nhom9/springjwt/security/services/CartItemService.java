d06991a0cb809c427fa16e67053dc7f54576d431
Anh Khoa <79366235+anhkhoa3302@users.noreply.github.com>
2022-12-07 19:45:45 +0700

    dashbroad api, cart item stock check

--- src/main/java/com/nhom9/springjwt/security/services/CartItemService.java
+++ src/main/java/com/nhom9/springjwt/security/services/CartItemService.java
@@ -7,15 +7,18 @@ import org.springframework.stereotype.Component;
 
 import com.nhom9.springjwt.models.CartItem;
 import com.nhom9.springjwt.payload.request.CartItemRequest;
+import com.nhom9.springjwt.payload.response.CartItemResponse;
 
 @Component
 public interface CartItemService {
-	CartItem createCartItem(CartItemRequest cartItemRequest);
+	CartItemResponse createCartItem(CartItemRequest cartItemRequest);
 
 	Optional<CartItem> updateCartItem(long cartItemId, CartItemRequest cartItem);
 
 	void deteleCartItem(long cartItemId);
 
 	List<CartItem> getCart(long userId);
+	
+
 
 }
--- src/main/java/com/nhom9/springjwt/security/services/CartItemServiceImpl.java
+++ src/main/java/com/nhom9/springjwt/security/services/CartItemServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.validator.internal.engine.messageinterpolation.parser.MessageDescriptorFormatException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.source.InvalidConfigurationPropertyValueException;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import com.nhom9.springjwt.models.Product;
 import com.nhom9.springjwt.models.User;
 import com.nhom9.springjwt.models.CartItem;
 import com.nhom9.springjwt.payload.request.CartItemRequest;
+import com.nhom9.springjwt.payload.response.CartItemResponse;
 import com.nhom9.springjwt.repository.CartItemRepository;
 import com.nhom9.springjwt.repository.ProductRepository;
 import com.nhom9.springjwt.repository.UserRepository;
@@ -26,22 +28,27 @@ public class CartItemServiceImpl implements CartItemService {
 	CartItemRepository cartItemRepo;
 
 	@Override
-	public CartItem createCartItem(CartItemRequest cartItemRequest) {
+	public CartItemResponse createCartItem(CartItemRequest cartItemRequest) {
 		// TODO Auto-generated method stub
+		User user = userRepo.findById(cartItemRequest.getUser_id()).orElseThrow();
+		Product product = productRepo.findById(cartItemRequest.getProduct_id()).orElseThrow();
+		CartItem cartItem = new CartItem(user, product, cartItemRequest.getQuantity());
+		if((product.getStock()<cartItemRequest.getQuantity())||(product.getStock()<=0))
+		{
+			return new CartItemResponse(cartItem, null, "Out of stocks!");
+		}
 		CartItem isInCart = cartItemRepo.findByUser_IdAndProduct_IdAndStatus(cartItemRequest.getUser_id(), cartItemRequest.getProduct_id(), 0);
-
 		if (isInCart != null) {
 			if (isInCart.getQuantity() + cartItemRequest.getQuantity() <= isInCart.getProduct().getStock())
 				isInCart.setQuantity(isInCart.getQuantity() + cartItemRequest.getQuantity());
 			else
 				isInCart.setQuantity(isInCart.getProduct().getStock());
-			return cartItemRepo.save(isInCart);
+			cartItemRepo.save(isInCart);
+			
+			return new CartItemResponse(isInCart, null, "Cart Updated");
 		} else {
-			User user = userRepo.findById(cartItemRequest.getUser_id()).orElseThrow();
-			Product product = productRepo.findById(cartItemRequest.getProduct_id()).orElseThrow();
-			CartItem cartItem = new CartItem(user, product, cartItemRequest.getQuantity());
-
-			return cartItemRepo.save(cartItem);
+			cartItemRepo.save(cartItem);
+			return new CartItemResponse(cartItem, null, "Cart Added");
 		}
 	}
 
